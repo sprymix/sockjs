f9727ca0649846fefc5dd3cb485d570b4291d29d
Elvis Pranskevichus <elprans@sprymix.com>
2015-07-29 13:52:01 -0400

    Revert "Fixed call SessionManager.aquire () - was removed the second, unnecessary argument."
    This reverts commit 0dfaa9dc08b35eb095bd0674eb953eda9069194c.

--- sockjs/transports/rawwebsocket.py
+++ sockjs/transports/rawwebsocket.py
@@ -50,7 +50,7 @@ class RawWebSocketTransport(Transport):
         ws.start(self.request)
 
         try:
-            yield from self.manager.acquire(self.session)
+            yield from self.manager.acquire(self.session, self)
         except:  # should use specific exception
             yield from ws.close(message='Go away!')
             return ws
